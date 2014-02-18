8b623b69a1a722563e01df39403bc9ea5ca0abcf
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-18 16:01:57 +0100

    javascript: examples fixed

--- software/examples/javascript/ExampleInterrupt.js
+++ software/examples/javascript/ExampleInterrupt.js
@@ -1,12 +1,11 @@
-var IPConnection = require('Tinkerforge/IPConnection');
-var BrickletIndustrialDigitalIn4 = require('Tinkerforge/BrickletIndustrialDigitalIn4');
+var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
 var UID = 'hxF';// Change to your UID
 
-var ipcon = new IPConnection();// Create IP connection
-var idi4 = new BrickletIndustrialDigitalIn4(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var idi4 = new Tinkerforge.BrickletIndustrialDigitalIn4(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
@@ -15,7 +14,7 @@ ipcon.connect(HOST, PORT,
 );// Connect to brickd
 
 // Don't use device before ipcon is connected
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Enable interrupt on pin 0
         idi4.setInterrupt(1 << 0);     
@@ -23,7 +22,7 @@ ipcon.on(IPConnection.CALLBACK_CONNECTED,
 );
 
 // Register callback for interrupts
-idi4.on(BrickletIndustrialDigitalIn4.CALLBACK_INTERRUPT,
+idi4.on(Tinkerforge.BrickletIndustrialDigitalIn4.CALLBACK_INTERRUPT,
     // Callback function for interrupts
     function(interruptMask, valueMask) {
         console.log('Interrupt by: '+parseInt(interruptMask).toString(2));
--- software/examples/javascript/ExampleSimple.js
+++ software/examples/javascript/ExampleSimple.js
@@ -1,12 +1,11 @@
-var IPConnection = require('Tinkerforge/IPConnection');
-var BrickletIndustrialDigitalIn4 = require('Tinkerforge/BrickletIndustrialDigitalIn4');
+var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
 var UID = 'hxF';// Change to your UID
 
-var ipcon = new IPConnection();// Create IP connection
-var idi4 = new BrickletIndustrialDigitalIn4(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection();// Create IP connection
+var idi4 = new Tinkerforge.BrickletIndustrialDigitalIn4(UID, ipcon);// Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
@@ -15,7 +14,7 @@ ipcon.connect(HOST, PORT,
 );// Connect to brickd
 
 // Don't use device before ipcon is connected
-ipcon.on(IPConnection.CALLBACK_CONNECTED,
+ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Read out values as bitmask
         idi4.getValue(
