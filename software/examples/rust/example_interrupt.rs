8e3ede924207311f52d5217c11ad5c1cd9776018
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:21:56 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_interrupt.rs
+++ software/examples/rust/example_interrupt.rs
@@ -1,31 +1,32 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{industrial_digital_in_4_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_digital_in_4_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Digital In 4 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Digital In 4 Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_digital_in_4_bricklet = IndustrialDigitalIn4Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let idi4 = IndustrialDigitalIn4Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for interrupt events.
-    let interrupt_listener = industrial_digital_in_4_bricklet.get_interrupt_receiver();
-    // Spawn thread to handle received events. This thread ends when the industrial_digital_in_4_bricklet
+    // Create receiver for interrupt events.
+    let interrupt_receiver = idi4.get_interrupt_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `idi4` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in interrupt_listener {
-            println!("Interrupt Mask: {:b}", event.interrupt_mask);
-            println!("Value Mask: {:b}", event.value_mask);
+        for interrupt in interrupt_receiver {
+            println!("Interrupt Mask: {:b}", interrupt.interrupt_mask);
+            println!("Value Mask: {:b}", interrupt.value_mask);
             println!();
         }
     });
 
     // Enable interrupt on pin 0
-    industrial_digital_in_4_bricklet.set_interrupt(1 << 0);
+    idi4.set_interrupt(1 << 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{industrial_digital_in_4_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_digital_in_4_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Digital In 4 Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Digital In 4 Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_digital_in_4_bricklet = IndustrialDigitalIn4Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let idi4 = IndustrialDigitalIn4Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current value as bitmask
-    let value_mask = industrial_digital_in_4_bricklet.get_value().recv()?;
+    // Get current value as bitmask.
+    let value_mask = idi4.get_value().recv()?;
     println!("Value Mask: {:b}", value_mask);
 
     println!("Press enter to exit.");
